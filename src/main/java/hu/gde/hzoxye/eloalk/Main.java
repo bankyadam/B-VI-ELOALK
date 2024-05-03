04bac3bb704262afbe4eaa046999f2a00ce1fd77
Adam Banky <adam.banky@byborgenterprises.com>
2024-05-03 23:33:08 +0200

    Create snails for the game

--- src/main/java/hu/gde/hzoxye/eloalk/Main.java
+++ src/main/java/hu/gde/hzoxye/eloalk/Main.java
@@ -1,16 +1,23 @@
 package hu.gde.hzoxye.eloalk;
 
 
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-        // create snails and add to board
+        // create snails
+        List<Snail> snails = List.of(
+                new Snail(1, Snail.Color.RED),
+                new Snail(2, Snail.Color.GREEN),
+                new Snail(3, Snail.Color.BLUE)
+        );
 
         // get bid for a snail
 
         // run each turn
-            // select a snail and add the booster
-            // step each snail
+        //      select a snail and add the booster
+        //      step each snail
         // print result
     }
 
