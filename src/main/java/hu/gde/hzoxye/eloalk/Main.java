da966faee1a1386e96d5a3d9de72ea4a415b0b96
Adam Banky <adam.banky@byborgenterprises.com>
2024-05-03 23:28:44 +0200

    Init with skeleton

---
+++ src/main/java/hu/gde/hzoxye/eloalk/Main.java
@@ -0,0 +1,17 @@
+package hu.gde.hzoxye.eloalk;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        // create snails and add to board
+
+        // get bid for a snail
+
+        // run each turn
+            // select a snail and add the booster
+            // step each snail
+        // print result
+    }
+
+}
